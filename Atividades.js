1b3475591fcce55ec084f6613fcf269558dc3aab
Mcr-Maria <ceissinha@gmail.com>
2025-04-23 16:45:14 -0300

    Atividades JAvaScript

---
+++ Atividades.js
@@ -0,0 +1,75 @@
+// //Questão 1
+// let idadeMinima = 17;
+// if(idadeMinima >=18){
+//     console.log("Maior de idade");
+// }else{
+//     console.log("Menor de idade")
+// }
+
+// //Questão 2
+// let numero = -10;
+
+// if(numero > 0){
+//     console.log("Positivo");
+
+// }else if( numero == 0 ){
+//         console.log("zero");
+
+// } else {
+//     console.log("Negativo");
+// } 
+
+// //Questão 3
+// let numeroA = 7;
+// let numeroB = 5;
+// if(numeroA > numeroB){
+//     console.log(`${numeroA} é maior que ${numeroB}`)
+// } else if(nuemroB > numeroA){
+//     console.log(`${numeroB} é maior que ${numeroA}`)  
+// } else {
+//     console.log("os numero são iguais");
+// }
+
+// //Questão 4
+// let numero = 42;
+
+// if((numero % 2) == 0){
+//     console.log(`${numero} é par!`);
+// } else {
+//     console.log(`${numero} é impar!`);
+// }
+
+// //Questão 5
+// let notaA = 7;
+// let notaB = 7;
+// let notaC = 8;
+// let media = ((notaA + notaB + notaC) /3).toFixed(1);
+// let situação = "";
+
+//     if(media >= 7) {
+//         situacao = "Aprovado";
+//     } else if (media > 5 && media < 7) {
+//         situacao = "Recuperação";
+//     } else {
+//         situacao = "Reprovado";
+//     }
+// console.log(`${situacao}, media: ${media}`);
+    
+// Questão 6
+
+// let valor = 620;
+// let desconto = 0.35;
+// if(valor > 100){
+// console.log(`total a pagar: R$ ${(valor - (valor * desconto)).toFixed(2)}`);
+
+// } else {
+// console.log(`total a pagar: R$ ${valor.toFixed(2)}`)
+// }
+ //questão 7
+
+ let ano = 2024
+ if (ano % 4 == 0) {
+    console.log("anobissexto");
+ } else {
+    console.log("Ano comum");
+ }
---
+++ index.js
@@ -0,0 +1,10 @@
+//solicita idade
+let idade = 18;
+//verifica se é maior que de idade
+if(idade > 18){
+    console.log("maior idade");
+}
+else(idade < 18)
+{
+    console.log("menor idade");
+}
